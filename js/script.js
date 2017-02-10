34882b6052ae68598dd82d4ca1847f9b060bb3e2
runlinhan <hanrunlin@126.com>
2017-02-10 08:54:11 -0500

    move

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$('.redbutton').on('click',function() {
+ $('.colorbox').css('background','red');
+ $('.message').text('Become an architect of thriving, socially- and economically-diverse communities. We’ll help you tap into the key elements of a healthy community ecosystem: from the economic theories behind real estate markets to the social theories underlying the importance of community organizations. And then we’ll help you layer on practical skills, including how to analyze current planning and policy issues and create new ones.')
+})
+
+$('.greenbutton').on('click',function() {
+ $('.colorbox').css('background','green');
+ $('.message').text('Environmental protections and healthy economic development don’t have to be at odds. There is a balance. We’ll help you find it, design for it, and deliver it. You’ll pull from existing theory and historical trends to develop your own frameworks for understanding and assessing the cultural values and institutional contexts at play in the give-and- take of environmentally-conscious development. What’s more, we’ll help you figure out how to engage public interest for long-term, community-supported impact.')
+})
+
+$('.bluebutton').on('click',function() {
+ $('.colorbox').css('background','blue');
+ $('.message').text('When “the greater good” has global proportions, how do you balance the needs and values of all the people involved? We’ll help you answer that. Whether you’re interested in planning for the multinational cities of tomorrow or the developing economies of today, we’ll prepare you to juggle the complexities of work with and across governments, NGOs, and international development agencies—and anything else the world ends up throwing your way.')
+})
